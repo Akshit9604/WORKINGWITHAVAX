665b304280c87129ed8f26051b6312fc4d464556
Akshit9604 <132479119+Akshit9604@users.noreply.github.com>
2024-09-30 22:56:21 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -5,9 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Mytoken is ERC20{
+contract Mytoken is ERC20 {
 
-    constructor() ERC20("TORQUE", "TOU") {}
+    // Mapping of item names (gaming tools) to required token amounts
+    mapping(string => uint256) public itemPrices;
+
+    constructor() ERC20("TORQUE", "TOU") {
+        // Initialize item prices
+        itemPrices["Health Potion"] = 100 * 10 ** decimals(); // 100 tokens
+        itemPrices["Mana Potion"] = 150 * 10 ** decimals();   // 150 tokens
+        itemPrices["Magic Sword"] = 300 * 10 ** decimals();   // 300 tokens
+        itemPrices["Shield of Valor"] = 250 * 10 ** decimals(); // 250 tokens
+        itemPrices["Epic Bow"] = 400 * 10 ** decimals();       // 400 tokens
+    }
 
     function decimals() public pure override returns (uint8) {
         return 10;
@@ -38,5 +48,14 @@ contract Mytoken is ERC20{
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         _burn(msg.sender, amount);
     }
-}
 
+    // Redeem an item for a specific amount of tokens
+    function redeemItem(string memory itemName) public {
+        uint256 price = itemPrices[itemName];
+        require(price > 0, "Item does not exist");
+        require(balanceOf(msg.sender) >= price, "Insufficient balance to redeem this item");
+
+        // Burn the required amount of tokens
+        _burn(msg.sender, price);
+    }
+}
